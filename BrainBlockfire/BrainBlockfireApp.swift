a473edfd62910b150a3e31ba02511f4988f2c7d6
Mario Ehiagwina <marioehiagwina03@icloud.com>
2023-05-24 14:17:11 +0200

    Initial Commit

---
+++ BrainBlockfire/BrainBlockfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBlockfireApp.swift
+//  BrainBlockfire
+//
+//  Created by Mario Ehiagwina on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct BrainBlockfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainBlockfireUITests/BrainBlockfireUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BrainBlockfireUITestsLaunchTests.swift
+//  BrainBlockfireUITests
+//
+//  Created by Mario Ehiagwina on 2023-05-24.
+//
+
+import XCTest
+
+final class BrainBlockfireUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
